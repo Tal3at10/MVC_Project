2837c00e4eb431a6ad2d7a3b539b308eaa56ea21
Tal3at10 <mt7993047@gmail.com>
2025-04-23 21:29:09 +0200

    EmployeeController - Delete

--- Demo.BLL/Dtos/Employees/EmployeeToReturnDto.cs
+++ Demo.BLL/Dtos/Employees/EmployeeToReturnDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,11 @@ namespace Demo.BLL.Dtos.Employees
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public int? Age { get; set; }
+
+        [DataType(DataType.Currency)]
         public decimal Salary { get; set; }
+
+        [DisplayName("IS active")]
         public bool IsActive { get; set; }
         public string? Email { get; set; }
         public string Gander { get; set; }
